ae6aeef6f183e94d51a5d8783ea6655d10f44794
Sergei Kosarev <sergeikosarev@Sergei-K.attlocal.net>
2023-02-25 13:18:17 -0800

    hof

--- src/App.js
+++ src/App.js
@@ -1,19 +1,67 @@
 import "./App.css";
-import { RadioGroup, RadioOption } from "./Radio";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+const MousePosition = ({ render }) => {
+  const [mousePosition, setMousePosition] = useState({
+    x: 0,
+    y: 0,
+    
+  });
+
+  useEffect(() => {
+    const handleMousePositionChange = (e) => {
+      setMousePosition({
+        x: e.clientX,
+        y: e.clientY,
+      })
+    };
+
+    window.addEventListener("mousemove", handleMousePositionChange);
+
+    return () => {
+      window.removeEventListener("mousemove", handleMousePositionChange);
+    };
+  }, []);
+
+  
+  return render({ mousePosition });
+};
+
+const PanelMouseLogger = ({mousePosition}) => {
+  return (
+    <div className="BasicTracker">
+      <p>Mouse position:</p>
+      <MousePosition
+       render={({ mousePosition }) => (
+      <div className="Row">
+        <span>x: {mousePosition.x}</span>
+        <span>y: {mousePosition.y}</span>
+      </div>
+     )}
+     />
+   </div>
+  );
+};
+
+const PointMouseLogger = ({mousePosition}) => {
+ 
+  return (
+    <MousePosition
+    render={({ mousePosition }) => (
+    <p>
+      ({mousePosition.x}, {mousePosition.y})
+    </p>
+    )}
+    />
+  )
+};
 
 function App() {
-  const [selected, setSelected] = useState("");
   return (
     <div className="App">
-      <h2>How did you hear about Little Lemon?</h2>
-      <RadioGroup onChange={setSelected} selected={selected}>
-        <RadioOption value="social_media">Social Media</RadioOption>
-        <RadioOption value="friends">Friends</RadioOption>
-        <RadioOption value="advertising">Advertising</RadioOption>
-        <RadioOption value="other">Other</RadioOption>
-      </RadioGroup>
-      <button disabled={!selected}>Submit</button>
+      <header className="Header">Big lemon 🍕</header>
+      <PanelMouseLogger />
+      <PointMouseLogger />
     </div>
   );
 }
